6ca8719b182960ceb5613dfea7e0559a141e0780
aarnavanoop <aarnavanoop@gmail.com>
2025-12-31 18:19:26 +1100

    Add accessor methods

--- 2.1P/MobileProgram/Program.cs
+++ 2.1P/MobileProgram/Program.cs
@@ -4,20 +4,48 @@ class MobileProgram
 {
     static void Main(string[] args)
     {
-        
+      Mobile jimMobile = new Mobile("Monthly", "Samsung Galaxy S6","0771223" );
+
+      Console.WriteLine("Account Type :" + jimMobile.getAccType()
+      + "\nMobile Number: " + jimMobile.getNumber() + "\nDevice: " + jimMobile.getDevice()
+      + "\nBalance: " + jimMobile.getBalance());
+
     }
 }
 
 class Mobile
 {
-    private string acctype, device, number;
+    private string accType, device, number;
     private double balance;
 
-    public Mobile(string acctype, string device, string number)
+    public Mobile(string accType, string device, string number)
     {
-        this.acctype = acctype;
+        this.accType = accType;
         this.device = device;
         this.number = number;
         this.balance = 0.0;
     }
+
+    //Accessor Methods
+    public string getAccType()
+    {
+        return this.accType;
+    }
+
+    public string getDevice()
+    {
+        return this.device;
+    }
+
+    public string getNumber()
+    {
+        return this.number;
+    }
+
+    public string getBalance()
+    {
+        return this.balance.ToString("C");
+    }
+
+
 }
