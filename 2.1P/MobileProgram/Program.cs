9d3022af63670e5a86e189300c7f23f4f74931c7
aarnavanoop <aarnavanoop@gmail.com>
2025-12-31 18:09:06 +1100

    Setup Mobile class and basic file structure

---
+++ 2.1P/MobileProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Problem1;
+
+class MobileProgram
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
+
+class Mobile
+{
+    private string acctype, device, number;
+    private double balance;
+
+    public Mobile(string acctype, string device, string number)
+    {
+        this.acctype = acctype;
+        this.device = device;
+        this.number = number;
+        this.balance = 0.0;
+    }
+}
