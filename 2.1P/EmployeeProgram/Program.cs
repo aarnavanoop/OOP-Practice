47e3bc405f884d8be3568dd12e9de59030eb2b75
aarnavanoop <aarnavanoop@gmail.com>
2026-01-01 18:29:10 +1100

    Tested that EmployeeProgram class works fine

--- 2.1P/EmployeeProgram/Program.cs
+++ 2.1P/EmployeeProgram/Program.cs
@@ -1,10 +1,40 @@
 ﻿namespace Problem2;
 
-class EmployeeProrgram
+class EmployeeProgram
 {
     static void Main(string[] args)
     {
-        
+        string? raiseInput = "";
+        bool isValid = false;
+        double raiseValue = 0;
+        string percentageToRaise = "";
+        double percentageRaise = 0;
+
+        Employee jimEmployee = new Employee("Jim", 10000);
+
+        Console.WriteLine($"Hello {jimEmployee.getName()}, you are earning a salary of {jimEmployee.getSalary():C}");
+        Console.WriteLine();
+
+        do
+        {
+
+            Console.WriteLine("What is the raise given to you?: ");
+            raiseInput = Console.ReadLine();
+            string[] percentageToRaiseArray =  raiseInput.Split("%");
+            percentageToRaise = percentageToRaiseArray[0];
+            isValid = double.TryParse(percentageToRaise, out raiseValue);
+            if (!isValid)
+            {
+                Console.WriteLine("You have to enter a number! ");
+                Console.WriteLine();
+            }
+
+        }while(!isValid);
+
+        jimEmployee.raiseSalary(raiseValue);
+
+        Console.ReadLine();
+
     }
 }
 
@@ -15,20 +45,24 @@ class Employee
 
     public Employee(string employeeName, double currentSalary)
     {
-        _employeeName = name;
+        _employeeName = employeeName;
         _currentSalary = currentSalary;
     }
 
-    public string getName(string employeeName)
+    public string getName()
     {
         return _employeeName;
     }
 
-    public double getSalary(double currentSalary)
+    public double getSalary()
     {
         return _currentSalary;
     }
 
-    
+    public void raiseSalary(double percentage)
+    {
+        _currentSalary = (_currentSalary * (percentage/100)) + _currentSalary;
+        Console.WriteLine($"Your salary after your raise is {getSalary():C}");
+    }
     
 }
