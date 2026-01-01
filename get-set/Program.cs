33fb8a44fdbf35ba373ec97abde29f42b17c48c8
aarnavanoop <aarnavanoop@gmail.com>
2026-01-02 00:06:16 +1100

    Practice get and set attributes and properties

---
+++ get-set/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+static void Main(string[] args)
+    {
+        Employee jim = new Employee("Jim", 5000);
+        jim.RaiseSalary(50);
+        Console.WriteLine(jim.Salary);
+    }
+}
+
+class Employee
+{
+    public string Name {get;}
+    public double Salary{get;private set;}
+
+    public Employee(string employeeName, double employeeSalary)
+    {
+        Name = employeeName;
+        Salary = employeeSalary;
+    }
+
+    public void RaiseSalary(double percentage)
+    {
+        Salary = ((percentage/100) * Salary) + Salary;
+    }
+
+}
