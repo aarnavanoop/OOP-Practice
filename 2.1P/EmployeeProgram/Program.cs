d56941a1192d6bbc93e801b0b73a56d11c3bfd32
aarnavanoop <aarnavanoop@gmail.com>
2026-01-01 17:48:28 +1100

    add accessor methods

--- 2.1P/EmployeeProgram/Program.cs
+++ 2.1P/EmployeeProgram/Program.cs
@@ -10,5 +10,25 @@ class EmployeeProrgram
 
 class Employee
 {
+    private string _employeeName;
+    private double _currentSalary;
+
+    public Employee(string employeeName, double currentSalary)
+    {
+        _employeeName = name;
+        _currentSalary = currentSalary;
+    }
+
+    public string getName(string employeeName)
+    {
+        return _employeeName;
+    }
+
+    public double getSalary(double currentSalary)
+    {
+        return _currentSalary;
+    }
+
+    
     
 }
