3f165fec92e72e189fb871fb68bd9d5d6f1a994f
aarnavanoop <aarnavanoop@gmail.com>
2026-01-04 17:37:01 +1100

    Add instance variables, constructors and first method

---
+++ 2.3C/MyTime/Program.cs
@@ -0,0 +1,73 @@
+﻿class MyTime
+{
+    private int _hour;
+    private int _minute;
+    private int _second;
+
+    public int Hour
+    {
+        get => _hour;
+
+        set
+        {
+            if(value < 0 || value > 23)
+            {
+                throw new ArgumentOutOfRangeException(nameof(Hour), "Hour value must be between 0 and 23.");
+            }
+            _hour = value;
+
+        }
+    }
+    public int Minute
+    {
+        get => _minute;
+        set
+        {
+            if(value < 0 || value > 59)
+            {
+                throw new ArgumentOutOfRangeException(nameof(Minute), "Minute value must be between 0 and 59");
+            }
+            _minute = value;
+
+        }
+    }
+    public int Second
+    {
+        get => _second;
+        set
+        {
+            if(value < 0 || value > 59)
+            {
+                throw new ArgumentOutOfRangeException(nameof(Second), "Second value must be between 0 and 59");
+            }
+            _second = value;
+
+        }
+    }
+
+    public MyTime()
+    {
+        Hour = 0;
+        Minute = 0;
+        Second = 0;
+
+    }
+    public MyTime(int hour, int minute, int second)
+    {
+        this.Hour = hour;
+        this.Minute = minute;
+        this.Second = second;
+    }
+
+    public void SetTime(int hour, int minute, int second)
+    {
+        this.Hour = hour;
+        this.Minute = minute;
+        this.Second = second;  
+    }
+
+    
+
+
+
+}
