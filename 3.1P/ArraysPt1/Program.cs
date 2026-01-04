7997d11c5ffec8a4c8bafdc9a049078da4068807
aarnavanoop <aarnavanoop@gmail.com>
2026-01-04 23:04:02 +1100

    Change file names and add arrays for 3.1P question 1

---
+++ 3.1P/ArraysPt1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ArraysPt1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] arrayOne = new double[10];
+        arrayOne[0] = 1.0;
+        arrayOne[1] = 1.1;
+        arrayOne[2] = 1.2;
+        arrayOne[3] = 1.3;
+        arrayOne[4] = 1.4;
+        arrayOne[5] = 1.5;
+        arrayOne[6] = 1.6;
+        arrayOne[7] = 1.7;
+        arrayOne[8] = 1.8;
+        arrayOne[9] = 1.9;
+        
+
+    }
+}
