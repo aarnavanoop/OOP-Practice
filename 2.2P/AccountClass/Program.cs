41cef5deaed2d15fbc92c04c3e4f07964660e3de
aarnavanoop <aarnavanoop@gmail.com>
2026-01-04 16:29:56 +1100

    Add account class

---
+++ 2.2P/AccountClass/Program.cs
@@ -0,0 +1,28 @@
+﻿class Account
+{
+    public decimal Balance {get;private set;}
+ 
+    public string Name {get;private set;}
+
+    public Account(string userName, decimal userBalance)
+    {
+        Name = userName;
+        Balance = userBalance;
+    }
+
+    public void Deposit (decimal amount)
+    {
+        Balance += amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        Balance -= amount;
+    }
+
+    public void Print()
+    {
+      Console.WriteLine($"The current user is {Name}");
+      Console.WriteLine($"The balance is {Balance}");
+    }
+}
