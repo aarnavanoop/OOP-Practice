e34d58c49c636d171ce896776e616845ecd62aa5
aarnavanoop <aarnavanoop@gmail.com>
2026-01-06 00:29:25 +1100

    Add account class from previous task

---
+++ 3.2P/BankSystem/Program.cs
@@ -0,0 +1,40 @@
+﻿class TestAccount
+{
+    static void Main(string[] args)
+    {
+        Account JimAccount = new Account("Jim", 1000);
+        JimAccount.Deposit(150);
+        JimAccount.Withdraw(100);
+        JimAccount.Print();
+    }
+}
+
+
+class Account
+{
+    public decimal Balance {get;private set;}
+ 
+    public string Name {get;private set;}
+
+    public Account(string userName, decimal userBalance)
+    {
+        Name = userName;
+        Balance = userBalance;
+    }
+
+    public void Deposit (decimal amount)
+    {
+        Balance += amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        Balance -= amount;
+    }
+
+    public void Print()
+    {
+      Console.WriteLine($"The current user is {Name}");
+      Console.WriteLine($"The balance is {Balance:C}");
+    }
+}
