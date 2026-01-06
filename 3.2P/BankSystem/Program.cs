5ef2afb13bdd9a5bf6ef3635748db13beab63d0d
aarnavanoop <aarnavanoop@gmail.com>
2026-01-06 23:27:00 +1100

    Add validation logic and menu for UI, for the user

--- 3.2P/BankSystem/Program.cs
+++ 3.2P/BankSystem/Program.cs
@@ -6,37 +6,111 @@ class BankSystem
 {
     static void Main(string[] args)
     {
-        static MenuOption ListOptions()
-        {
-            do
-            {
-                Console.WriteLine("----- Please pick from one of these options ----");
-                Console.WriteLine($"{(int)MenuOption.Withdraw}: {MenuOption.Withdraw}");
-                Console.WriteLine($"{(int)MenuOption.Deposit}: {MenuOption.Deposit}");
-                Console.WriteLine($"{(int)MenuOption.Print}: {MenuOption.Print}");
-                Console.WriteLine($"{(int)MenuOption.Quit}: {MenuOption.Quit}");
-                Console.WriteLine("Enter your choice: ");
 
-                string userChoice = Console.ReadLine();
-
-                if(Enum.TryParse<MenuOption>(userChoice, true, out MenuOption choice))
-                {
-                    if(Enum.IsDefined(typeof(MenuOption), choice))
-                    {
-                        return choice;
-                    }
-                }
-
-                Console.WriteLine("Invalid choice, please try again.");
-
-            }while(true);
-        }
-
-        ListOptions();
         Account JimAccount = new("Jim", 1000);
-        JimAccount.Deposit(100);
-        JimAccount.Withdraw(150);
-        JimAccount.Print();
+        bool keepRunning = true;
+        while (keepRunning)
+        {
+            MenuOption choice = ReadUserOptions();  
+            switch (choice)
+            {
+                case MenuOption.Withdraw:
+                    DoWithdraw(JimAccount);
+                    break;
+                case MenuOption.Deposit:
+                    DoDeposit(JimAccount);
+                    break;
+                case MenuOption.Print:
+                    JimAccount.Print();
+                    break;
+                case MenuOption.Quit:
+                    Console.WriteLine("Goodbye!");
+                    keepRunning = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid selection");
+                    break;
+            }  
+        }
+        Console.WriteLine("Program exited successfuly");
+    }
+
+    static void DoDeposit(Account account)
+    {
+        bool transactionSuccesful = false;
+        do
+        {
+            Console.WriteLine("How much would you like to deposit?: ");
+            string? userInput = Console.ReadLine();
+
+            if(decimal.TryParse(userInput, out decimal amount) && amount > 0)
+            {
+                transactionSuccesful = account.Deposit(amount);
+                if (transactionSuccesful)
+                {
+                    Console.WriteLine("Deposit succesful");
+                }
+                else
+                {
+                    Console.WriteLine("Deposit rejected by the system");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid positive integer");
+            }
+        }while(!transactionSuccesful);
+    }
+
+    static void DoWithdraw(Account account)
+    {
+        bool transactionSuccesful = false;
+        do
+        {
+            Console.WriteLine("How much would you like to withdraw?: ");
+            string? userInput = Console.ReadLine();
+
+            if(decimal.TryParse(userInput, out decimal amount) && amount > 0)
+            {   
+                transactionSuccesful = account.Withdraw(amount);
+                if(!transactionSuccesful)
+                {
+                    Console.WriteLine("Withdrawal failed. Insufficient funds or invalid value");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid positive number");
+            }
+        }while(!transactionSuccesful);
+
+        Console.WriteLine("Withdrawal succesful.");
+    }
+
+    static void DoPrint(Account account)
+    {
+        account.Print();
+    }
+    static MenuOption ReadUserOptions()
+    {
+        do
+        {
+            Console.WriteLine("----- Please pick from one of these options ----");
+            Console.WriteLine($"{(int)MenuOption.Withdraw}: {MenuOption.Withdraw}");
+            Console.WriteLine($"{(int)MenuOption.Deposit}: {MenuOption.Deposit}");
+            Console.WriteLine($"{(int)MenuOption.Print}: {MenuOption.Print}");
+            Console.WriteLine($"{(int)MenuOption.Quit}: {MenuOption.Quit}");
+            Console.WriteLine("Enter your choice: ");
+            string userChoice = Console.ReadLine();
+            if(Enum.TryParse<MenuOption>(userChoice, true, out MenuOption choice))
+            {
+                if(Enum.IsDefined(typeof(MenuOption), choice))
+                {
+                    return choice;
+                }
+            }
+            Console.WriteLine("Invalid choice, please try again.");
+        }while(true);
     }
 }
 
