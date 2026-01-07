493f72d64ea5c6deb6d70e7bf58834beca2097d1
aarnavanoop <aarnavanoop@gmail.com>
2026-01-08 01:25:12 +1100

    Add NullReferenceException example and question answers

---
+++ 4.1P/NullReferenceException/NullReferenceException.cs
@@ -0,0 +1,29 @@
+﻿namespace TaskFourOne;
+
+class CatchTheError
+{
+    static void Main(string[] args)
+    {
+        string userInput = null;
+        int checkedLength;
+        try
+        {
+            checkedLength = ThrowTheError.CheckLength(userInput);
+        }
+        catch (NullReferenceException)
+        {
+            Console.WriteLine("The string you provided(userInput) was null, the check length method does not work on a null value");
+        }
+    }
+}
+
+class ThrowTheError
+{
+    public static int CheckLength(string userInput)
+    {   
+        int checkedLength = userInput.Length;
+        return checkedLength;
+    }
+  
+}
+
