b5a5e4c927986ae56a2b46b2891c11ae6f66bfe5
aarnavanoop <aarnavanoop@gmail.com>
2026-01-12 21:14:21 +1100

    Add overloaded methods

---
+++ 5.1P/Overloading/Program.cs
@@ -0,0 +1,17 @@
+﻿class Overloading
+{
+    static void Main(string[] args)
+    {
+        methodToBeOverloaded("Peter");
+    }
+
+    public static void methodToBeOverloaded(string name)
+    {
+        Console.WriteLine("Name: " + name);
+    }
+
+    public static void methodToBeOverloaded(string name, int age)
+    {
+        Console.WriteLine($"Name: {name} \nAge: {age}");
+    }
+}
