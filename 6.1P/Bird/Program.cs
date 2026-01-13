4556775ed21e66e535c394cbf2e147f6906e9298
aarnavanoop <aarnavanoop@gmail.com>
2026-01-13 23:35:48 +1100

    Setup initial bird class

---
+++ 6.1P/Bird/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
+class Bird
+{
+    public string Name {get;private set;}  
+
+    public Bird(string name)
+    {
+        Name = name;
+    }
+
+    public virtual void fly()
+    {
+        Console.WriteLine("Bird is flapping");
+    }
+
+    public override string ToString()
+    {
+        return $"{Name}";
+    }
+
+     
+}
