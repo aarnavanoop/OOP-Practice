1395fa16597f26af177511fc7215cc493a26ccf7
aarnavanoop <aarnavanoop@gmail.com>
2026-01-13 23:50:50 +1100

    Add inherited classes Duck and Penguin

--- 6.1P/Bird/Program.cs
+++ 6.1P/Bird/Program.cs
@@ -4,7 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        Bird bird = new Bird("Joel");
+        Console.WriteLine(bird.ToString());
+        bird.fly();
+
+        Penguin penguin = new Penguin("Pingu");
+        Console.WriteLine(penguin.ToString());
+        penguin.fly();
+
+        Duck duck = new Duck("Ducky", 50, "ugly");
+        Console.WriteLine(duck.ToString());
+        duck.fly();
+
     }
 }
 class Bird
@@ -18,13 +29,48 @@ class Bird
 
     public virtual void fly()
     {
-        Console.WriteLine("Bird is flapping");
+        Console.WriteLine("Flap, Flap, Flap");
     }
 
     public override string ToString()
     {
-        return $"{Name}";
+        return $"A bird called {Name}";
+    }
+     
+}
+
+class Penguin : Bird
+{
+    public Penguin(string name) : base(name)
+    {
+
+    }
+    public override void fly()
+    {
+        Console.WriteLine("Penguin can't really fly..");
     }
 
-     
+    public override string ToString()
+    {
+        return $"A penguin called {Name}";
+    }
+}
+
+class Duck : Bird
+{
+    public double Size {get;private set;}
+    public string Kind {get;private set;}
+
+    public Duck(string name, double size, string kind)
+        : base(name)
+    {
+        Size = size;
+        Kind = kind;
+    }
+
+    public override string ToString()
+    {
+        return $"A duck called {Name} of size {Size} and kind {Kind}";
+    }
+
 }
