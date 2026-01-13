7b24a9c5fdf158ac9f052388e7e260b232b3fad0
aarnavanoop <aarnavanoop@gmail.com>
2026-01-14 01:22:06 +1100

    Add Interface example from BroCode

---
+++ Interfaces/InterfacesPractice/BroCode/Program.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Rabbit rabbit = new Rabbit();
+        rabbit.Flee();
+
+        Hawk hawk = new Hawk();
+        hawk.Hunt();
+    }
+}
+
+interface IPrey
+{
+   void Flee();
+}
+
+interface IPredator
+{
+   void Hunt() ;
+}
+
+class Rabbit:IPrey
+{
+    public void Flee()
+    {
+        Console.WriteLine("The rabbit runs away");
+    }
+}
+
+class Hawk: IPredator
+{
+    public void Hunt()
+    {
+        Console.WriteLine("The Hawk hunts");
+    }
+}
+
+class Fish
+{
+    
+}
