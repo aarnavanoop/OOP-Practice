bd6d59aee163f4e46d21dbf98abc93502127e954
aarnavanoop <aarnavanoop@gmail.com>
2026-01-16 03:22:36 +1100

    Add logic for the Add method

--- MyPolynomial/Program.cs
+++ MyPolynomial/Program.cs
@@ -12,13 +12,17 @@ class Program
         double evaluatedPolynomial = polynomialOne.Evaluate(4);
         Console.WriteLine(evaluatedPolynomial);
 
+        double[] coefficients2 = {1,2,3,4,5,6};
+        MyPolynomial another = new MyPolynomial(coefficients2);
+        
+        MyPolynomial trial = polynomialOne.Add(another);
+        Console.WriteLine(trial.ToString());
+
     }
 }
 class MyPolynomial
 {
     public double[] Coeffs {get;set;}
-
-
     public MyPolynomial(double[] coeffs)
     {
         Coeffs = coeffs;
@@ -60,4 +64,40 @@ class MyPolynomial
         return valueOfPolynomial;
     }
 
+    public MyPolynomial Add(MyPolynomial another)
+    {
+        int lengthOriginal = Coeffs.Length;
+        int lengthNew = another.Coeffs.Length;
+        int greaterLength = Math.Max(lengthOriginal, lengthNew);
+
+        double[] addedPolynomials = new double[greaterLength];
+
+        double[] longArray;
+        double[] shorterArray;
+
+        if(lengthOriginal >= lengthNew)
+        {
+            longArray = Coeffs;
+            shorterArray = another.Coeffs;
+        }
+        else
+        {
+            shorterArray = Coeffs;
+            longArray = another.Coeffs;  
+        }
+
+        for(int i = 0; i < longArray.Length; i++)
+        {
+            addedPolynomials[i] = longArray[i];
+        }
+
+        int indexOffset = longArray.Length - shorterArray.Length;
+        for(int i = indexOffset; i < longArray.Length; i++)
+        {
+            addedPolynomials[i] += shorterArray[i - indexOffset];
+        }
+
+        MyPolynomial afterAdding = new MyPolynomial(addedPolynomials);
+        return afterAdding;
+    }
 }
