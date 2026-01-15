15f6a705d60302c2142b50ec8bf3505326ad032c
aarnavanoop <aarnavanoop@gmail.com>
2026-01-16 00:52:38 +1100

    Update logic for ToString method

--- MyPolynomial/Program.cs
+++ MyPolynomial/Program.cs
@@ -7,51 +7,82 @@ class Program
     {
         double[] coefficients = {1,2,3,4};
         MyPolynomial polynomialOne = new MyPolynomial(coefficients);
-        string[] convertedpolynomial = polynomialOne.ConvertArray(polynomialOne.Coeffs);
         Console.WriteLine(polynomialOne.ToString());
+        Console.WriteLine(polynomialOne.ToString());
+
+        /*double evaluatedPolynomial = polynomialOne.Evaluate(4);
+        Console.WriteLine(evaluatedPolynomial);*/
 
     }
 }
 class MyPolynomial
 {
     public double[] Coeffs {get;set;}
-    public int arrayLength {get;set;}
-
-    public string[] ConvertedArray{get;set;}
+    //public int arrayLength {get;set;}
+    //public string[] ConvertedArray{get;set;}
 
     public MyPolynomial(double[] coeffs)
     {
         Coeffs = coeffs;
-        arrayLength = coeffs.Length;
-        ConvertedArray = new string[arrayLength];
+        //arrayLength = coeffs.Length;
+        //ConvertedArray = new string[arrayLength];
 
     }
 
     public int GetDegree()
     {
-        return arrayLength - 1;
+        return Coeffs.Length - 1;
     }
 
-    public string[] ConvertArray(double[] coeffs)
+    public bool CheckReverse(double[] coeffs)
     {
-        //string[] ConvertedArray = new string[arrayLength];
-        if(coeffs[0] < coeffs[arrayLength -1])
+        if(coeffs[0] < coeffs[coeffs.Length - 1])
         {
-            Array.Reverse(coeffs);
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    public string[] ConvertArray()
+    {
+        string[] ConvertedArray = new string[Coeffs.Length];
+        if (!CheckReverse(Coeffs))
+        {
+            Array.Reverse(Coeffs);
         }
 
-        for(int i = 0; i < arrayLength; i++)
+        for(int i = 0; i < Coeffs.Length; i++)
         {
             string stringVersion = Coeffs[i].ToString();
-            ConvertedArray[i] = stringVersion + $"x^{arrayLength - 1 - i}";
+            ConvertedArray[i] = stringVersion + $"x^{Coeffs.Length - 1 - i}";
         }
 
         return ConvertedArray;
     }
-   public override string ToString()
+    public override string ToString()
     {
-        return String.Join(" + ", ConvertedArray);
+        return String.Join(" + ", ConvertArray());
     }
-    
 
+    /*public double Evaluate(double x)
+    {
+        double valueOfPolynomial = 0;
+        double currentTerm = 0;
+        for(int i = 0; i < arrayLength; i++)
+        {
+            if(i == 0)
+            {
+              valueOfPolynomial += Coeffs[i];  
+            }
+            else
+            {
+                currentTerm = Math.Pow(x,i) * Coeffs[i];
+                valueOfPolynomial += currentTerm;
+            }
+        }
+
+        return valueOfPolynomial;
+    }*/
 }
